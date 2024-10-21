bd785c398e1820fee39afc6eb20fe828c93631ae
Thomas Stokes <git@tombl.dev>
2024-10-21 15:47:17 +0800

    move from patch functions to part objects
    allows them to be stateful, fixing the event handler bug

--- html.js
+++ html.js
@@ -11,25 +11,130 @@ function memo(fn) {
 	}
 }
 
+class ChildPart {
+	#childIndex
+	constructor(idx) {
+		this.#childIndex = idx
+	}
+
+	#node
+	create(node, value) {
+		this.#node = node.childNodes[this.#childIndex]
+		this.update(value)
+	}
+
+	update(value) {
+		if (typeof value !== 'object') value = document.createTextNode(value)
+		console.assert(value instanceof Node, 'Expected a Node, got', value)
+		this.#node.replaceWith(value)
+		this.#node = value
+	}
+}
+
+class EventPart {
+	#name
+	constructor(name) {
+		this.#name = name
+	}
+
+	#attached = false
+	#handler = {}
+	#node
+	create(node, value) {
+		this.#node = node
+		this.update(value)
+	}
+
+	update(value) {
+		this.#handler.handleEvent = value
+		if (typeof value === 'function') {
+			if (!this.#attached) {
+				this.#node.addEventListener(this.#name, this.#handler)
+				this.#attached = true
+			}
+		} else if (value === null) {
+			if (this.#attached) {
+				this.#node.removeEventListener(this.#name, this.#handler)
+				this.#attached = false
+			}
+		} else {
+			throw new Error(`Expected a function or null, got ${value}`)
+		}
+	}
+}
+
+class PropertyPart {
+	#name
+	constructor(name) {
+		this.#name = name
+	}
+
+	#node
+	create(node, value) {
+		this.#node = node
+		this.update(value)
+	}
+
+	update(value) {
+		this.#node[this.#name] = value
+	}
+}
+
+class BooleanAttributePart {
+	#name
+	constructor(name) {
+		this.#name = name
+	}
+
+	#node
+	create(node, value) {
+		this.#node = node
+		this.update(value)
+	}
+
+	update(value) {
+		this.#node.toggleAttribute(this.#name, value)
+	}
+}
+
+class AttributePart {
+	#name
+	constructor(name) {
+		this.#name = name
+	}
+
+	#node
+	create(node, value) {
+		this.#node = node
+		this.update(value)
+	}
+
+	update(value) {
+		if (value === null) this.#node.removeAttribute(this.#name)
+		else this.#node.setAttribute(this.#name, value)
+	}
+}
+
 const DYNAMIC_WHOLE = /^dyn-\$(\d+)$/i
 const DYNAMIC_GLOBAL = /dyn-\$(\d+)/gi
 const compileTemplate = memo(statics => {
 	const templateElement = document.createElement('template')
 	templateElement.innerHTML = statics.reduce((a, v, i) => a + v + (i === statics.length - 1 ? '' : `dyn-$${i}`), '')
 
-	let nextPatch = 0
-	const patches = Array(statics.length - 1)
-	function patch(node, idx, fn) {
-		node.dataset.dynPatch ??= ''
-		node.dataset.dynPatch += ' ' + nextPatch
-		if (nextPatch !== idx) console.warn('dynamic value detected in static location')
-		patches[nextPatch++] = (node, dynamics) => fn(node, dynamics[idx])
+	let nextPart = 0
+	const parts = Array(statics.length - 1)
+	function patch(node, idx, part) {
+		if ('dynParts' in node.dataset) node.dataset.dynParts += ' ' + nextPart
+		else node.dataset.dynParts = nextPart
+		if (nextPart !== idx) console.warn('dynamic value detected in static location')
+		parts[nextPart++] = [idx, part]
 	}
 
 	for (
-		let node, walker = document.createTreeWalker(templateElement.content, NodeFilter.SHOW_TEXT | NodeFilter.SHOW_ELEMENT);
-		(node = walker.nextNode()) && nextPatch < patches.length;
+		let walker = document.createTreeWalker(templateElement.content, NodeFilter.SHOW_TEXT | NodeFilter.SHOW_ELEMENT);
+		nextPart < parts.length && walker.nextNode();
 	) {
+		const node = walker.currentNode
 		if (node.nodeType === Node.TEXT_NODE) {
 			const nodes = []
 			for (const match of [...node.data.matchAll(DYNAMIC_GLOBAL)].reverse()) {
@@ -42,11 +147,7 @@ const compileTemplate = memo(statics => {
 			let siblings
 			for (const [node, idx] of nodes) {
 				const child = (siblings ??= [...node.parentElement.childNodes]).indexOf(node)
-				patch(node.parentElement, parseInt(idx), (node, value) => {
-					node = node.childNodes[child]
-					if (typeof value !== 'object') value = document.createTextNode(value)
-					node.replaceWith(value)
-				})
+				patch(node.parentElement, parseInt(idx), () => new ChildPart(child))
 			}
 		} else {
 			const toRemove = []
@@ -58,15 +159,7 @@ const compileTemplate = memo(statics => {
 						name = name.slice(1)
 						const match = DYNAMIC_WHOLE.exec(value)
 						if (match === null) throw new Error('`@` attributes must be functions')
-						patch(node, parseInt(match[1]), (node, value) => {
-							if (typeof value === 'function') {
-								node.addEventListener(name, value)
-							} else if (value === null) {
-								node.removeEventListener(name, value)
-							} else {
-								throw new Error(`Expected a function or null, got ${value}`)
-							}
-						})
+						patch(node, parseInt(match[1]), () => new EventPart(name))
 						break
 					}
 
@@ -76,9 +169,7 @@ const compileTemplate = memo(statics => {
 						name = name.slice(1)
 						const match = DYNAMIC_WHOLE.exec(value)
 						if (match === null) throw new Error('`.` attributes must be properties')
-						patch(node, parseInt(match[1]), (node, value) => {
-							node[name] = value
-						})
+						patch(node, parseInt(match[1]), () => new PropertyPart(name))
 						break
 					}
 
@@ -88,9 +179,7 @@ const compileTemplate = memo(statics => {
 						name = name.slice(1)
 						const match = DYNAMIC_WHOLE.exec(value)
 						if (match === null) throw new Error('`?` attributes must be booleans')
-						patch(node, parseInt(match[1]), (node, value) => {
-							node.toggleAttribute(name, value)
-						})
+						patch(node, parseInt(match[1]), () => new BooleanAttributePart(name))
 						break
 					}
 
@@ -98,9 +187,7 @@ const compileTemplate = memo(statics => {
 					default: {
 						const match = DYNAMIC_WHOLE.exec(value)
 						if (match === null) continue
-						patch(node, parseInt(match[1]), (node, value) => {
-							node.setAttribute(name, value)
-						})
+						patch(node, parseInt(match[1]), () => new AttributePart(name))
 					}
 				}
 			}
@@ -108,36 +195,39 @@ const compileTemplate = memo(statics => {
 		}
 	}
 
-	patches.length = nextPatch
+	parts.length = nextPart
 
-	return { content: templateElement.content, patches }
+	return { content: templateElement.content, parts }
 })
 
-export function html(statics, ...dynamics) {
-	return { [TAG_TEMPLATE]: compileTemplate(statics), [TAG_DYNAMICS]: dynamics }
-}
+export const html = (statics, ...dynamics) => ({
+	[TAG_TEMPLATE]: compileTemplate(statics),
+	[TAG_DYNAMICS]: dynamics,
+})
 
 class TemplateInstance {
 	constructor(template, dynamics, range) {
-		this.patches = template.patches
 		const doc = template.content.cloneNode(true)
-		this.elements = []
+		const elementByPart = []
 
-		for (const el of doc.querySelectorAll('[data-dyn-patch]')) {
-			const patches = el.dataset.dynPatch
-			delete el.dataset.dynPatch
-			for (const patch of patches.slice(1).split('  ')) this.elements[patch] = el
+		for (const el of doc.querySelectorAll('[data-dyn-parts]')) {
+			const parts = el.dataset.dynParts
+			delete el.dataset.dynParts
+			for (const part of parts.split(' ')) elementByPart[part] = el
 		}
 
-		this.update(dynamics)
+		this.parts = template.parts.map(([dynamicIdx, createPart], elementIdx) => {
+			const part = createPart()
+			part.create(elementByPart[elementIdx], dynamics[dynamicIdx])
+			return part
+		})
+
 		range.deleteContents()
 		range.insertNode(doc)
 	}
+
 	update(dynamics) {
-		for (let i = 0; i < this.patches.length; i++) {
-			const replacement = this.patches[i](this.elements[i], dynamics)
-			if (replacement !== undefined) this.elements[i] = replacement
-		}
+		for (const [part, idx] of this.parts) part.update(dynamics[idx])
 	}
 }
 
@@ -152,7 +242,7 @@ export class Root {
 	}
 
 	render({ [TAG_TEMPLATE]: template, [TAG_DYNAMICS]: dynamics }) {
-		if (this.#instance?.patches === template.patches) {
+		if (this.#instance?.parts === template.parts) {
 			this.#instance.update(dynamics)
 		} else {
 			this.#instance = new TemplateInstance(template, dynamics, this.#range)
