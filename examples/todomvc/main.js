6a0dd036ca6ffba20fea2249e8f60c584d27ef3d
Thomas Stokes <git@tombl.dev>
2024-10-26 23:15:45 +0800

    migrate todo item to it's own class

--- examples/todomvc/main.js
+++ examples/todomvc/main.js
@@ -17,84 +17,89 @@ function classes(node, value) {
 	}
 }
 
-function autofocus(node) {
-	node.focus()
-	node.setSelectionRange(0, node.value.length)
-}
+const autofocus = node => node.focus()
+const autoselect = node => node.setSelectionRange(0, node.value.length)
 
-function item({ app, todo }) {
-	return html`
-		<li ${classes}=${[todo.completed && 'completed', todo.editing && 'editing']}>
-			<div class="view">
-				<input
-					class="toggle"
-					type="checkbox"
-					checked=${todo.completed}
-					@change=${e => {
-						e.preventDefault()
-						app.update(todo.id, todo => (todo.completed = e.target.checked))
-					}}
-				/>
-				<label @dblclick=${() => app.update(todo.id, todo => (todo.editing = true))}>${todo.title}</label>
-				<button class="destroy" @click=${() => app.remove(todo.id)}></button>
-			</div>
-			${todo.editing
-				? html`
-						<div class="input-container">
-							<input
-								class="edit"
-								value=${todo.title}
-								${autofocus}
-								@blur=${e => {
-									const value = e.target.value.trim()
-									if (value) {
-										app.update(todo.id, todo => {
-											todo.title = value
-											todo.editing = false
-										})
-									}
-								}}
-								@keydown=${e => {
-									if (e.key === 'Enter') {
+class TodoItem {
+	id = crypto.randomUUID()
+	completed = false
+	editing = false
+	constructor(app, title) {
+		this.app = app
+		this.title = title
+	}
+
+	render(controller) {
+		return html`
+			<li ${classes}=${[this.completed && 'completed', this.editing && 'editing']}>
+				<div class="view">
+					<input
+						class="toggle"
+						type="checkbox"
+						checked=${this.completed}
+						@change=${e => {
+							e.preventDefault()
+							this.completed = e.target.checked
+							controller.invalidate()
+						}}
+					/>
+					<label
+						@dblclick=${() => {
+							this.editing = true
+							controller.invalidate()
+						}}
+						>${this.title}</label
+					>
+					<button
+						class="destroy"
+						@click=${() => {
+							this.app.remove(this.id)
+							this.app.controller.invalidate()
+						}}
+					></button>
+				</div>
+				${this.editing
+					? html`
+							<div class="input-container">
+								<input
+									class="edit"
+									value=${this.title}
+									${autofocus}
+									${autoselect}
+									@blur=${e => {
 										const value = e.target.value.trim()
 										if (value) {
-											app.update(todo.id, todo => {
-												todo.title = value
-												todo.editing = false
-											})
+											this.title = value
+											this.editing = false
+											controller.invalidate()
 										}
-									}
-								}}
-							/>
-						</div>
-				  `
-				: null}
-		</li>
-	`
+									}}
+									@keydown=${e => {
+										if (e.key === 'Enter') {
+											const value = e.target.value.trim()
+											if (value) {
+												this.title = value
+												this.editing = false
+												controller.invalidate()
+											}
+										}
+									}}
+								/>
+							</div>
+					  `
+					: null}
+			</li>
+		`
+	}
 }
 
 class App {
 	todos = []
-	#nextId = 0
-	add(title) {
-		this.todos.push({
-			id: this.#nextId++,
-			title,
-			completed: false,
-			editing: false,
-		})
-		this.controller?.invalidate()
-	}
 	get(id) {
 		return this.todos.find(todo => todo.id === id)
 	}
-	update(id, cb) {
-		cb(this.get(id))
-		this.controller?.invalidate()
-	}
 	remove(id) {
 		this.todos = this.todos.filter(todo => todo.id !== id)
-		this.controller?.invalidate()
 	}
 
 	render(controller) {
@@ -111,8 +116,9 @@ class App {
 						if (event.key === 'Enter') {
 							const value = event.target.value.trim()
 							if (value) {
-								this.add(value)
+								this.todos.push(new TodoItem(this, value))
 								event.target.value = ''
+								controller.invalidate()
 							}
 						}
 					}}
@@ -125,7 +131,7 @@ class App {
 								<input class="toggle-all" type="checkbox" checked=${this.todos.every(todo => todo.completed)} />
 							</div>
 							<ul class="todo-list">
-								${this.todos.map(todo => item({ app: this, todo }))}
+								${this.todos}
 							</ul>
 						</main>
 				  `
@@ -134,7 +140,13 @@ class App {
 	}
 }
 
-Root.appendInto(document.getElementById('root')).render((globalThis.app = new App()))
+const app = new App()
+globalThis.app = app
+document.body.addEventListener('keypress', e => {
+	if (e.ctrlKey && e.key === 'i') app.controller?.invalidate()
+})
 
-app.add('hello')
-app.add('world')
+app.todos.push(new TodoItem(app, 'hello'))
+app.todos.push(new TodoItem(app, 'world'))
+
+Root.appendInto(document.getElementById('root')).render(app)
