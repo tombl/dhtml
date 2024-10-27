9594b5099cb43d3904de2710c274777b29e63d00
Thomas Stokes <git@tombl.dev>
2024-10-27 16:43:56 +0800

    test runner: add timing, serial execution, warmup run

--- test/custom-element-compat.js
+++ test/custom-element-compat.js
@@ -1,20 +1,20 @@
 import { Root, html } from '../html.js'
 import { assert } from './_lib.js'
 
+class CustomElement extends HTMLElement {
+	#thing
+	get thing() {
+		return this.#thing
+	}
+	set thing(value) {
+		this.#thing = value.toUpperCase()
+	}
+}
+
+customElements.define('custom-element', CustomElement)
+
 export default root => {
 	const r = Root.appendInto(root)
-
-	class CustomElement extends HTMLElement {
-		#thing
-		get thing() {
-			return this.#thing
-		}
-		set thing(value) {
-			this.#thing = value.toUpperCase()
-		}
-	}
-
-	customElements.define('custom-element', CustomElement)
 
 	r.render(html`<custom-element .thing=${'hello'}></custom-element>`)
 	assert(root.firstElementChild instanceof CustomElement)
