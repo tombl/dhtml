821b2010acc677d6d264e30826efcba86a82fadf
Thomas Stokes <git@tombl.dev>
2024-10-27 14:28:16 +0800

    add failing test: list reordering

---
+++ test/list-reorder.js
@@ -0,0 +1,19 @@
+import { Root, html } from '../html.js'
+import { assert } from './_lib.js'
+
+export default root => {
+	const r = Root.appendInto(root)
+
+	const items = [html`<h1>Item 1</h1>`, html`<h2>Item 2</h2>`]
+
+	r.render(items)
+	assert.eq(root.innerHTML, '<h1>Item 1</h1><h2>Item 2</h2>')
+	const original = [...root.children]
+
+	;[items[0], items[1]] = [items[1], items[0]]
+
+	r.render(items)
+	assert.eq(root.innerHTML, '<h2>Item 2</h2><h1>Item 1</h1>')
+	assert.eq(root.children[0], original[1])
+	assert.eq(root.children[1], original[0])
+}
