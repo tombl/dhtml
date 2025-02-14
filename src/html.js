2867604e0eb39ab7f634f2eb69e5a72449a3e4c9
Thomas Stokes <git@tombl.dev>
2025-02-14 20:04:44 +0800

    list keying

--- src/html.js
+++ src/html.js
@@ -3,6 +3,7 @@
 	CustomPartConstructor,
 	CustomPartInstance,
 	Displayable,
+	Key,
 	Part,
 	Renderable,
 	Span as SpanInstance
@@ -139,6 +140,8 @@ class BoundTemplateInstance {
 }
 
 export class Root {
+	/** @type {Key | undefined} */ _key
+
 	/** @param {Span} span */
 	constructor(span) {
 		this._span = span
@@ -387,6 +390,13 @@ export function getParentNode(renderable) {
 	return controller._parentNode
 }
 
+const keys = new WeakMap()
+export function keyed(renderable, key) {
+	if (keys.has(renderable)) throw new Error('renderable already has a key')
+	keys.set(renderable, key)
+	return renderable
+}
+
 /** @implements {Part} */
 class ChildPart {
 	#childIndex
@@ -499,14 +509,42 @@ class ChildPart {
 			let i = 0
 			let end = this.#span._end
 			for (const item of value) {
-				const root = (this.#roots[i++] ??= Root.insertAfter(end))
+				// @ts-expect-error -- WeakMap lookups of non-objects always return undefined, which is fine
+				const key = keys.get(item) ?? item
+				const root = (this.#roots[i] ??= Root.insertAfter(end))
+
+				if (key !== undefined && root._key !== key) {
+					const j = this.#roots.findIndex(r => r._key === key)
+					root._key = key
+					if (j !== -1) {
+						const root1 = root
+						const root2 = this.#roots[j]
+
+						// swap the contents of the spans
+						const content1 = root2._span._extractContents()
+						const content2 = root1._span._extractContents()
+						root2._span._insertNode(content2)
+						root1._span._insertNode(content1)
+
+						// swap the spans back
+						;[root1._span, root2._span] = [root2._span, root1._span]
+
+						// swap the roots
+						this.#roots[i] = root2
+						this.#roots[j] = root1
+					}
+				}
+
 				root.render(item)
 				end = root._span._end
+
+				i++
 			}
 
 			// and now remove excess roots if the iterable has shrunk.
 			while (this.#roots.length > i) {
-				const root = /** @type {Root} */ (this.#roots.pop())
+				const root = this.#roots.pop()
+				assert(root)
 				root.detach()
 				root._span._deleteContents()
 			}
--- test/lists.test.ts
+++ test/lists.test.ts
@@ -1,10 +1,6 @@
-import { html, type Displayable } from 'dhtml'
+import { html, keyed, type Displayable } from 'dhtml'
 import { describe, expect, it } from 'vitest'
 import { setup } from './setup'
-
-function keyed<T extends Displayable>(displayable: T, _key: unknown) {
-	return displayable
-}
 
 function shuffle<T>(array: T[]) {
 	for (let i = 0; i < array.length; i++) {
@@ -84,7 +80,7 @@ describe('lists', () => {
 		expect(el.innerHTML).toBe('[]')
 	})
 
-	it.todo('swap', () => {
+	it('swap', () => {
 		const { root, el } = setup()
 
 		const items = [html`<p>Item 1</p>`, html`<p>Item 2</p>`, html`<p>Item 3</p>`]
@@ -127,7 +123,7 @@ describe('lists', () => {
 		expect(el.children[2]).toBe(item3)
 	})
 
-	it.todo('shift', () => {
+	it('shift', () => {
 		const { root, el } = setup()
 
 		const items = [html`<p>Item 1</p>`, html`<p>Item 2</p>`, html`<p>Item 3</p>`]
@@ -154,7 +150,7 @@ describe('lists', () => {
 	})
 })
 
-describe.todo('list reordering', () => {
+describe('list reordering', () => {
 	it('unkeyed', () => {
 		const { root, el } = setup()
 
