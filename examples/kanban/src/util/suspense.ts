f8329eaeaa86103af7ca79322e71cfeaf471f4b2
Thomas Stokes <git@tombl.dev>
2025-03-09 23:23:03 +0800

    oops

--- examples/kanban/src/util/suspense.ts
+++ examples/kanban/src/util/suspense.ts
@@ -7,6 +7,7 @@ export function suspend<T>(promise: Promise<T>): T {
   const result = results.get(promise)
   switch (result?.state) {
     case undefined:
+      results.set(promise, { state: 'attached' })
       throw promise.then(
         value => results.set(promise, { state: 'resolved', value }),
         error => results.set(promise, { state: 'rejected', error }),
