4687335044cf3004aba93ee51ec2368f7c4eec50
Thomas Stokes <git@tombl.dev>
2025-03-10 22:22:29 +0800

    bundle the server build

--- build.sh
+++ build.sh
@@ -2,13 +2,17 @@
 
 mkdir -p dist
 
-esbuild src/html.js --bundle --minify --format=esm --define:DHTML_PROD=true --mangle-props=^_ --drop:console --drop-labels=DEV |
-terser --mangle --compress --module --output dist/html.min.js
-printf "min:    %d bytes\n" "$(wc -c <dist/html.min.js)"
+build() {
+    esbuild src/$1.js --bundle --minify --format=esm --define:DHTML_PROD=true --mangle-props=^_ --drop:console --drop-labels=DEV |
+    terser --mangle --compress --module --output dist/$1.min.js
+    printf "min:    %d bytes\n" "$(wc -c <dist/$1.min.js)"
 
-gzip --best <dist/html.min.js >dist/html.min.js.gz
-printf "gzip:   %d bytes\n" "$(wc -c <dist/html.min.js.gz)"
+    gzip --best <dist/$1.min.js >dist/$1.min.js.gz
+    printf "gzip:   %d bytes\n" "$(wc -c <dist/$1.min.js.gz)"
 
-brotli --best <dist/html.min.js >dist/html.min.js.br
-printf "brotli: %d bytes\n" "$(wc -c <dist/html.min.js.br)"
- 
+    brotli --best <dist/$1.min.js >dist/$1.min.js.br
+    printf "brotli: %d bytes\n" "$(wc -c <dist/$1.min.js.br)"
+}
+
+build html
+build html.server
