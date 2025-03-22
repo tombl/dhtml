2bd4b6b9d51b01eac0853972b28cb8a0fd1283e1
Thomas Stokes <git@tombl.dev>
2025-03-22 20:39:45 +0800

    dts buddy

--- src/client/html.ts
+++ src/client/html.ts
@@ -2,13 +2,15 @@ import type { Displayable } from '../shared.ts'
 import { compile_template, type CompiledTemplate } from './compiler.ts'
 import { assert, DEV } from './internal.ts'
 
+const tag = Symbol()
+
 interface HTML {
-	$: typeof html
-	_dynamics: unknown[]
-	_template: CompiledTemplate
+	[tag]: true
+	/* @internal */ _dynamics: unknown[]
+	/* @internal */ _template: CompiledTemplate
 }
 
-export const is_html = (value: any): value is HTML => value?.$ === html
+export const is_html = (value: any): value is HTML => typeof value === 'object' && value !== null && tag in value
 
 export function html(statics: TemplateStringsArray, ...dynamics: unknown[]): HTML {
 	let template: CompiledTemplate
@@ -21,7 +23,7 @@ export function html(statics: TemplateStringsArray, ...dynamics: unknown[]): HTM
 	}
 
 	return {
-		$: html,
+		[tag]: true,
 		_dynamics: dynamics,
 		get _template() {
 			return (template ??= compile_template(statics))
