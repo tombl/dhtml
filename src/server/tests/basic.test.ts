5e44e9248990aa977ea992abe0cf90f31259cd23
Thomas Stokes <git@tombl.dev>
2025-03-23 18:44:53 +0800

    add tests

--- src/server/tests/basic.test.ts
+++ src/server/tests/basic.test.ts
@@ -5,8 +5,8 @@ import assert from 'node:assert/strict'
 
 const dev_test = test.skipIf(!__DEV__)
 
-test('basic html renders correctly', () => {
-	assert.equal(renderToString(html`<h1>Hello, world!</h1>`), '<h1>Hello, world!</h1>')
+test('basic html renders correctly', async () => {
+	assert.equal(await renderToString(html`<h1>Hello, world!</h1>`), '<h1>Hello, world!</h1>')
 })
 
 test('basic html renders correctly via stream', async () => {
@@ -14,62 +14,62 @@ test('basic html renders correctly via stream', async () => {
 	assert.equal(await new Response(stream).text(), '<h1>Hello, world!</h1>')
 })
 
-test('inner content renders correctly', () => {
-	assert.equal(renderToString(html`<h1>${html`Inner content!`}</h1>`), '<h1>Inner content!</h1>')
+test('inner content renders correctly', async () => {
+	assert.equal(await renderToString(html`<h1>${html`Inner content!`}</h1>`), '<h1>Inner content!</h1>')
 })
 
-test('template with number renders correctly', () => {
+test('template with number renders correctly', async () => {
 	const template = (n: number) => html`<h1>Hello, ${n}!</h1>`
-	assert.equal(renderToString(template(1)), '<h1>Hello, 1!</h1>')
-	assert.equal(renderToString(template(2)), '<h1>Hello, 2!</h1>')
+	assert.equal(await renderToString(template(1)), '<h1>Hello, 1!</h1>')
+	assert.equal(await renderToString(template(2)), '<h1>Hello, 2!</h1>')
 })
 
-test('lists of items', () => {
-	assert.equal(renderToString([1, 'a', html`<span>thing</span>`]), '1a<span>thing</span>')
+test('lists of items', async () => {
+	assert.equal(await renderToString([1, 'a', html`<span>thing</span>`]), '1a<span>thing</span>')
 })
 
-test('basic children render correctly', () => {
+test('basic children render correctly', async () => {
 	assert.equal(
-		renderToString(html`<span>${'This is a'}</span> ${html`test`} ${html`test`} ${html`test`}`),
+		await renderToString(html`<span>${'This is a'}</span> ${html`test`} ${html`test`} ${html`test`}`),
 		'<span>This is a</span> test test test',
 	)
 })
 
-dev_test('invalid part placement raises error', () => {
-	assert.throws(() => renderToString(html`<${'div'}>${'text'}</${'div'}>`))
+dev_test('invalid part placement raises error', async () => {
+	await assert.rejects(async () => await renderToString(html`<${'div'}>${'text'}</${'div'}>`))
 })
 
-test('parts in comments do not throw', () => {
-	renderToString(html`<!-- ${'text'} -->`)
+test('parts in comments work', async () => {
+	assert.equal(await renderToString(html`<!-- ${'text'} -->`), '<!-- text -->')
 })
 
-dev_test('manually specifying internal template syntax throws', () => {
-	assert.throws(() => {
-		renderToString(html`${1} dyn-$0$`)
+dev_test('manually specifying internal template syntax throws', async () => {
+	await assert.rejects(async () => {
+		await renderToString(html`${1} dyn-$0$`)
 	})
 })
 
-test('syntax close but not exact does not throw', () => {
-	assert.equal(renderToString(html`dyn-$${0}1$`), 'dyn-$01$')
+test('syntax close but not exact does not throw', async () => {
+	assert.equal(await renderToString(html`dyn-$${0}1$`), 'dyn-$01$')
 })
 
-test('directives', () => {
+test('directives', async () => {
 	const directive = mock()
-	assert.equal(renderToString(html`<p ${directive}></p>`), '<p ></p>')
+	assert.equal(await renderToString(html`<p ${directive}></p>`), '<p ></p>')
 	assert.equal(directive.mock.calls.length, 1)
 	assert.deepEqual(directive.mock.calls[0], [])
 })
 
-test('attributes', () => {
-	assert.equal(renderToString(html`<a href=${'/url'}></a>`), '<a href="/url"></a>')
-	assert.equal(renderToString(html`<details hidden=${false}></details>`), '<details ></details>')
-	assert.equal(renderToString(html`<details hidden=${true}></details>`), '<details hidden></details>')
+test('attributes', async () => {
+	assert.equal(await renderToString(html`<a href=${'/url'}></a>`), '<a href="/url"></a>')
+	assert.equal(await renderToString(html`<details hidden=${false}></details>`), '<details ></details>')
+	assert.equal(await renderToString(html`<details hidden=${true}></details>`), '<details hidden></details>')
 })
 
-test('collapses whitespace', () => {
+test('collapses whitespace', async () => {
 	// prettier-ignore
-	assert.equal(renderToString(html`      <p>         </p>      `), ' <p> </p> ')
+	assert.equal(await renderToString(html`      <p>         </p>      `), ' <p> </p> ')
 
 	// prettier-ignore
-	assert.equal(renderToString(html`      <p>    x    </p>      `), ' <p>    x    </p> ')
+	assert.equal(await renderToString(html`      <p>    x    </p>      `), ' <p>    x    </p> ')
 })
