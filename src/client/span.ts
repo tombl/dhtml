4605a18cb6714fe7e24d8a8e25921a1259a74439
Thomas Stokes <git@tombl.dev>
2025-03-24 21:33:15 +0800

    devirtualize a bunch of methods

--- src/client/span.ts
+++ src/client/span.ts
@@ -1,4 +1,5 @@
 import { assert } from '../shared.ts'
+import { append_child, get_next_sibling, insert_before, remove_child } from './dom.ts'
 import { is_document_fragment } from './util.ts'
 
 export interface Span {
@@ -21,14 +22,15 @@ export function create_span(node: Node): Span {
 export function insert_node(span: Span, node: Node): void {
 	const end = is_document_fragment(node) ? node.lastChild : node
 	if (end === null) return // empty fragment
-	span._parent.insertBefore(node, span._end.nextSibling)
+	insert_before(span._parent, node, get_next_sibling(span._end))
 	span._end = end
 
 	if (span._start === span._marker) {
-		assert(span._start.nextSibling)
-		span._start = span._start.nextSibling
+		const sibling = get_next_sibling(span._start)
+		assert(sibling)
+		span._start = sibling
 
-		span._parent.removeChild(span._marker)
+		remove_child(span._parent, span._marker)
 		span._marker = null
 	}
 }
@@ -36,7 +38,7 @@ export function insert_node(span: Span, node: Node): void {
 function* nodes(span: Span): Generator<Node, void, unknown> {
 	let node = span._start
 	for (;;) {
-		const next = node.nextSibling
+		const next = get_next_sibling(node)
 		yield node
 		if (node === span._end) return
 		assert(next, 'expected more siblings')
@@ -45,21 +47,22 @@ function* nodes(span: Span): Generator<Node, void, unknown> {
 }
 
 export function extract_contents(span: Span): DocumentFragment {
-	span._marker = new Text()
-	span._parent.insertBefore(span._marker, span._start)
+	span._marker = document.createTextNode('')
+	insert_before(span._parent, span._marker, span._start)
 
 	const fragment = document.createDocumentFragment()
-	for (const node of nodes(span)) fragment.appendChild(node)
+
+	for (const node of nodes(span)) append_child(fragment, node)
 
 	span._start = span._end = span._marker
 	return fragment
 }
 
 export function delete_contents(span: Span): void {
-	span._marker = new Text()
-	span._parent.insertBefore(span._marker, span._start)
+	span._marker = document.createTextNode('')
+	insert_before(span._parent, span._marker, span._start)
 
-	for (const node of nodes(span)) span._parent.removeChild(node)
+	for (const node of nodes(span)) remove_child(span._parent, node)
 
 	span._start = span._end = span._marker
 }
--- src/client/util.ts
+++ src/client/util.ts
@@ -1,17 +1,19 @@
+import { get_node_type } from './dom.ts'
+
 export type Cleanup = (() => void) | void | undefined | null
 
 export function is_element(node: Node): node is Element {
-	return node.nodeType === (1 satisfies typeof Node.ELEMENT_NODE)
+	return get_node_type(node) === (1 satisfies typeof Node.ELEMENT_NODE)
 }
 
 export function is_text(node: Node): node is Text {
-	return node.nodeType === (3 satisfies typeof Node.TEXT_NODE)
+	return get_node_type(node) === (3 satisfies typeof Node.TEXT_NODE)
 }
 
 export function is_comment(node: Node): node is Comment {
-	return node.nodeType === (8 satisfies typeof Node.COMMENT_NODE)
+	return get_node_type(node) === (8 satisfies typeof Node.COMMENT_NODE)
 }
 
 export function is_document_fragment(node: Node): node is DocumentFragment {
-	return node.nodeType === (11 satisfies typeof Node.DOCUMENT_FRAGMENT_NODE)
+	return get_node_type(node) === (11 satisfies typeof Node.DOCUMENT_FRAGMENT_NODE)
 }
