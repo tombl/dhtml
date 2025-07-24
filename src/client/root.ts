fb99b56024131a15a11b64333d5716d7bb5bec97
Thomas Stokes <git@tombl.dev>
2025-07-24 20:49:46 +0800

    add hydrate api
    just as a shim that clears the parent currently

--- src/client/root.ts
+++ src/client/root.ts
@@ -1,11 +1,20 @@
-import type { Displayable } from '../shared.ts'
+import { assert, type Displayable } from '../shared.ts'
 import { create_child_part_inner } from './parts.ts'
-import { create_span_into } from './span.ts'
+import { create_span_into, type Span } from './span.ts'
 
 export interface Root {
 	render(value: Displayable): void
 }
 
 export function createRoot(parent: Node): Root {
-	return { render: create_child_part_inner(() => create_span_into(parent)) }
+	const span = create_span_into(parent)
+	return { render: create_child_part_inner(() => span) }
+}
+
+export function hydrate(parent: Node, value: Displayable): Root {
+	assert(parent.firstChild && parent.lastChild)
+	const span: Span = { _parent: parent, _start: parent.firstChild, _end: parent.lastChild }
+	const render = create_child_part_inner(() => span)
+	render(value)
+	return { render }
 }
