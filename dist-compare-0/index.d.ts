f5485567a2aa99ecdce87a6ebeabaa7fcee6eba3
Thomas Stokes <git@tombl.dev>
2025-08-01 13:43:47 +0800

    Use worktree/dist directly instead of copying

--- dist-compare-0/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { Displayable, HTML, Renderable, html } from './index2.js'
-export { Displayable, HTML, Renderable, html }
--- dist-compare-1/server.d.ts
+++
@@ -1,8 +0,0 @@
-import { Displayable } from './index2.js'
-
-//#region src/server.d.ts
-declare function renderToString(value: Displayable): string
-declare function renderToReadableStream(value: Displayable): ReadableStream<Uint8Array>
-//#endregion
-export { renderToReadableStream, renderToString }
-//# sourceMappingURL=server.d.ts.map
